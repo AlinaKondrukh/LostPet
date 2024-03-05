b06fbb80c79b279676c615ffcedac9ccdbd7ed6b
Alina <alia.kondruh@gmail.com>
2024-03-05 12:33:24 +0300

    С помощью gii генерироуем модель User

---
+++ models/User.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property int $role_id
+ *
+ * @property PetRequests[] $petRequests
+ * @property Role $role
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'password', 'role_id'], 'required'],
+            [['role_id'], 'integer'],
+            [['name'], 'string', 'max' => 511],
+            [['email', 'password'], 'string', 'max' => 255],
+            [['email'], 'unique'],
+            [['role_id'], 'exist', 'skipOnError' => true, 'targetClass' => Role::class, 'targetAttribute' => ['role_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'role_id' => 'Role ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[PetRequests]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPetRequests()
+    {
+        return $this->hasMany(PetRequests::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Role]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRole()
+    {
+        return $this->hasOne(Role::class, ['id' => 'role_id']);
+    }
+}
