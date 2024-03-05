e5910e78a1ade0ffce852f24d145b34e728c2157
Alina <alia.kondruh@gmail.com>
2024-03-05 15:45:36 +0300

    Форма просмотра и записи заявлений

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -65,7 +65,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <main id="main" class="flex-shrink-0" role="main">
     <div class="container">
         <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['homeLink' => ['label' => 'Дом', 'url' => '/'], 'links' => $this->params['breadcrumbs']]) ?>
+            <?= Breadcrumbs::widget(['homeLink' => ['label' => 'Главная', 'url' => '/'], 'links' => $this->params['breadcrumbs']]) ?>
         <?php endif ?>
         <?= Alert::widget() ?>
         <?= $content ?>
